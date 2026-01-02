e5c6f63847290a81bd7e07c6c5d1fd78694241c8
gowtham <gowthamm8743@gmail.com>
2026-01-02 13:36:40 -0500

    Fix ES module error (Render compatible)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   const fetchPortfolioData = async () => {
     try {
       // Try to fetch from backend
-      const response =  await axios.get(`${API_URL}/api/portfolio`, {
+      const response =  await axios.get(`https://portfolio-23-s578.onrender.com/api/portfolio`, {
       timeout: 5000
       })
       setPortfolioData(response.data)
